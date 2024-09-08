882c3ff07e84ca73b610e16f38d9e83c300d30a6
Vaidehi0207 <vaidehi.priya02@gmail.com>
2024-09-09 00:26:22 +0530

    Contact page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './home/Home'
 import {Route, Routes} from 'react-router-dom'
 import Courses from './courses/Courses'
 import Signup from './components/Signup'
+import Contact from './contact/Contact'
 
 const App = () => {
   return (
@@ -14,7 +15,7 @@ const App = () => {
     <Route path="/"element={<Home/>}/>
     <Route path="/course" element={<Courses/>}/>
     <Route path="/signup" element={<Signup/>}/>
-
+    <Route path="/contact" element={<Contact/>}/>
   </Routes>
   </div>
   </>
